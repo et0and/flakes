bdaeceaece5bb3145a8e1ef69b276bdfe8b20185
et0and <gh@tomhackshaw.com>
2026-01-11 22:04:21 +1300

    chore: clean up

--- flake.nix
+++ flake.nix
@@ -2,12 +2,10 @@
   description = "Tom's Nix flake";
 
   inputs = {
-    # NixOS official package source
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11"; # Use your current version
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11"; 
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    # Replace 'my-hostname' with your actual hostname (run 'hostname' to check)
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
